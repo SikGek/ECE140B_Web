fc5045b1159e0afac1f34372bc87950f0a9cab60
SikGek <81644416+SikGek@users.noreply.github.com>
2022-04-18 09:21:25 -0700

    Update server.py

--- web/src/server.py
+++ web/src/server.py
@@ -13,9 +13,9 @@ def hello_world(request):
       print('Incoming request')
       return FileResponse('templates/index.html') # the HTML file to be shown
 def productf(request):
-      return FileResponse('product.html')
+      return FileResponse('templates/product.html')
 def KVPf(request):
-      return FileResponse('KVP.html')
+      return FileResponse('templates/KVP.html')
 
 
 if __name__ == '__main__':
